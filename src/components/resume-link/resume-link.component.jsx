582174fd3a3c6693da6fee4af24cc6dbe6be5c99
dineshdevlpr <dineshiaf30@gmail.com>
2021-09-25 04:02:58 +0530

    resume updated

--- src/components/resume-link/resume-link.component.jsx
+++ src/components/resume-link/resume-link.component.jsx
@@ -5,7 +5,7 @@ import { ReactComponent as ResumeIcon } from '../../assets/ResumeIcon.svg';
 
 const ResumeLink = ({hoverRight}) => {
     return (
-        <Link className='resume-link-wrapper' to={{pathname: 'https://docs.google.com/document/d/1t4VSst_stGvIYIpzbtEm-UypEdDc-GtY7REnAfDWzKc/edit?usp=sharing' }} target="_blank" >
+        <Link className='resume-link-wrapper' to={{pathname: 'https://drive.google.com/file/d/1cW1Sdn1mc2pPvzlXJF6BRfK6CNryxZoY/view?usp=sharing' }} target="_blank" >
                 <div className={`${hoverRight ? 'resume-link-container hover-right' : 'resume-link-container'}`}>
                     <p className='resume-link-content'>Resume</p>
                     <ResumeIcon className='resume-logo'/>
